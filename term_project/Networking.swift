c0857a35bb6f3a964b5a4f13339016bea4b77b22
JPL-ST-SPRING2021 <jpl-st-spring2021@JPLSTSP2021sMBP.hsd1.ca.comcast.net>
2022-04-08 13:52:47 -0700

    create weather model, initial commit

---
+++ term_project/Networking.swift
@@ -0,0 +1,21 @@
+//
+//  Networking.swift
+//  term_project
+//
+//  Created by JPL-ST-SPRING2021 on 4/8/22.
+//
+
+import Foundation
+
+struct Networking {
+    
+    let baseURLString = "https://api.openweathermap.org/data/2.5/weather?"
+    let api_key = "boop"
+    
+    func fetchWeather(lat: String, lon: String) async throws -> WeatherModel {
+        let url = URL(string: "\(baseURLString)lat=\(lat)&lon=\(lon)&appid=\(api_key)")!
+        let (data,_) = try await URLSession.shared.data(from: url)
+        let decoder = JSONDecoder()
+        return try decoder.decode(WeatherModel.self, from: data)
+    }
+}
