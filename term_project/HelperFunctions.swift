e229b2dcaab97d4df1b889286afab3ff65413ad1
Kyleg97 <kyle.gilbert1622@gmail.com>
2022-04-08 22:04:08 -0700

    add helper function for kelvin to F

---
+++ term_project/HelperFunctions.swift
@@ -0,0 +1,12 @@
+//
+//  HelperFunctions.swift
+//  term_project
+//
+//  Created by JPL-ST-SPRING2021 on 4/8/22.
+//
+
+import Foundation
+
+func kelvinToFahrenheit(temperature: Double) -> Double {
+    return (temperature - 273.15) * 9/5 + 32
+}
