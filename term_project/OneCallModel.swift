6c0af7f4fdf0efab9afa7958ca51c71703634156
Kyleg97 <kyle.gilbert1622@gmail.com>
2022-04-20 19:52:26 -0700

    add potential model for uvi & weather

--- term_project/OneCallModel.swift
+++ term_project/OneCallModel.swift
@@ -1,8 +1,63 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
 //
-//  OneCallModel.swift
-//  term_project
-//
-//  Created by JPL-ST-SPRING2021 on 4/19/22.
-//
+//   let oneCallModel = try? newJSONDecoder().decode(OneCallModel.self, from: jsonData)
 
 import Foundation
+
+// MARK: - OneCallModel
+struct OneCallModel: Codable {
+    let lat, lon: Double?
+    let timezone: String?
+    let timezoneOffset: Int?
+    let current: Current?
+    let minutely: [Minutely]?
+
+    enum CodingKeys: String, CodingKey {
+        case lat, lon, timezone
+        case timezoneOffset = "timezone_offset"
+        case current, minutely
+    }
+}
+
+// MARK: - Current
+struct Current: Codable {
+    let dt, sunrise, sunset: Int?
+    let temp, feelsLike: Double?
+    let pressure, humidity: Int?
+    let dewPoint, uvi: Double?
+    let clouds, visibility: Int?
+    let windSpeed: Double?
+    let windDeg: Int?
+    let windGust: Double?
+    let weather: [Weathering]?
+
+    enum CodingKeys: String, CodingKey {
+        case dt, sunrise, sunset, temp
+        case feelsLike = "feels_like"
+        case pressure, humidity
+        case dewPoint = "dew_point"
+        case uvi, clouds, visibility
+        case windSpeed = "wind_speed"
+        case windDeg = "wind_deg"
+        case windGust = "wind_gust"
+        case weather
+    }
+}
+
+// MARK: - Weather
+struct Weathering: Codable {
+    let id: Int?
+    let main, weatherDescription, icon: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id, main
+        case weatherDescription = "description"
+        case icon
+    }
+}
+
+// MARK: - Minutely
+struct Minutely: Codable {
+    let dt, precipitation: Int?
+}
